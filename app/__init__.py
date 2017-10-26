9ae7a799617ec04ab2eef14763663f8d98242870
Ruoyu-Li <liruoyu0401@outlook.com>
2017-10-26 01:39:03 -0400

    Flask directory

---
+++ app/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config.from_object('config')
+Bootstrap(app)
+db = SQLAlchemy(app)
+
+from app import views, models
