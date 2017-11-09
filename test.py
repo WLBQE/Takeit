2594459b07360b835e3a2841e5918e3f6eb94e64
Ruoyu-Li <liruoyu0401@outlook.com>
2017-11-09 16:52:59 -0500

    more test cases

--- test.py
+++ test.py
@@ -1,6 +1,5 @@
 import unittest
 from app import app
-from app.models import User
 
 
 class Tests(unittest.TestCase):
@@ -22,13 +21,13 @@ class Tests(unittest.TestCase):
             remember=True
             ), follow_redirects=True)
 
-    def create_event(self, name, location, start_time, end_time, description):
+    def create_event(self, name, start_time, end_time, location, description):
         return self.app.post('/create_event', data=dict(
             name=name,
-            description=description,
+            start_date=start_time,
+            end_date=end_time,
             location=location,
-            start_time=start_time,
-            end_time=end_time,
+            description=description
         ))
 
     def test_login_success(self):
@@ -37,22 +36,66 @@ class Tests(unittest.TestCase):
         self.assertIn(b'Falun Dafa', rv.data)
         self.assertIn(b'8964', rv.data)
 
-        rv = self.app.get('/event_detail/2', follow_redirects=True)
-        self.assertEqual(rv.status_code, 200)
-        self.assertIn(b'1997-10-01', rv.data)
-
-        rv = self.app.get('/profile/1', follow_redirects=True)
-        self.assertEqual(rv.status_code, 200)
-        self.assertIn(b'19 Da', rv.data)
-
-        rv = self.app.get('/home', follow_redirects=True)
-        self.assertEqual(rv.status_code, 200)
-        self.assertIn(b'8964', rv.data)
-
     def test_login_fail(self):
         rv = self.login('xjp@ccp.gov', 'wrong_password')
         self.assertEqual(rv.status_code, 200)
         self.assertIn(b'sign in', rv.data)
+
+    def test_logout(self):
+        rv = self.login('xjp@ccp.gov', 'qwer1234')
+        self.assertEqual(rv.status_code, 200)
+        rv = self.app.get('/logout', follow_redirects=True)
+        self.assertEqual(rv.status_code, 200)
+        self.assertIn(b'sign in', rv.data)
+
+    def test_event_detail_no_session(self):
+        rv = self.app.get('/event_detail/2', follow_redirects=True)
+        self.assertEqual(rv.status_code, 200)
+        self.assertIn(b'sign in', rv.data)
+
+    def test_event_detail_exist(self):
+        rv = self.login('xjp@ccp.gov', 'qwer1234')
+        self.assertEqual(rv.status_code, 200)
+        rv = self.app.get('/event_detail/2', follow_redirects=True)
+        self.assertEqual(rv.status_code, 200)
+        self.assertIn(b'1997-10-01', rv.data)
+
+    def test_event_detail_non_exist(self):
+        rv = self.login('xjp@ccp.gov', 'qwer1234')
+        self.assertEqual(rv.status_code, 200)
+        rv = self.app.get('/event_detail/100', follow_redirects=True)
+        self.assertEqual(rv.status_code, 404)
+
+    def test_profile_no_session(self):
+        rv = self.app.get('/event_detail/2', follow_redirects=True)
+        self.assertEqual(rv.status_code, 200)
+        self.assertIn(b'sign in', rv.data)
+
+    def test_profile_exist(self):
+        rv = self.login('xjp@ccp.gov', 'qwer1234')
+        self.assertEqual(rv.status_code, 200)
+        rv = self.app.get('/profile/1', follow_redirects=True)
+        self.assertEqual(rv.status_code, 200)
+        self.assertIn(b'19 Da', rv.data)
+
+    def test_profile_non_exist(self):
+        rv = self.login('xjp@ccp.gov', 'qwer1234')
+        self.assertEqual(rv.status_code, 200)
+        rv = self.app.get('/profile/100', follow_redirects=True)
+        self.assertEqual(rv.status_code, 404)
+
+    def test_create_event_no_session(self):
+        rv = self.create_event('20 Da', '2022-10-01 10:00', '2022-10-07 10:00', 'Renmindahuitang', 'hello world')
+        self.assertEqual(rv.status_code, 302)
+        self.assertIn(b'login', rv.data)
+
+    def test_create_event(self):
+        rv = self.login('xjp@ccp.gov', 'qwer1234')
+        self.assertEqual(rv.status_code, 200)
+        rv = self.create_event('20 Da', '2022-10-01 10:00', '2022-10-07 10:00', 'Renmindahuitang', 'hello world')
+        self.assertEqual(rv.status_code, 302)
+        rv = self.app.get('/profile/1', follow_redirects=True)
+        self.assertIn(b'20 Da', rv.data)
 
 
 if __name__ == "__main__":
